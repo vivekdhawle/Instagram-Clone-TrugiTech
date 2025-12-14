37f4ed0829c5cb1deb8525c370d889b9fd545041
vivekdhawle <vivekdhawle02@gmail.com>
2025-12-14 18:11:10 +0530

    try3

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,26 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
 
-const app=express();
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+import profileRoutes from "./routes/profileRoutes.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors({origin: "*",
+    credentials: true}));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+connectDB();
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/profile", profileRoutes)
 
 
 export default app;
